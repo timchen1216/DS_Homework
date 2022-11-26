6112b9ac94db27c3ee31c6987a1c77dc6413bdeb
timchen1216 <timchengood@gmail.com>
2022-11-26 21:08:31 +0800

    11/26

--- HW4/main.cpp
+++ HW4/main.cpp
@@ -5,22 +5,27 @@
 #include <vector>
 #include "huffman.h"
 #include "arcd.h"
+#include <time.h>
 using namespace std;
 
 int main()
 {
-	char arr[] = { 'a', 'b', 'c', 'd', 'e' };
-	int freq[] = { 5, 10, 20, 25, 40 }; //freq 0.05 0.1 0.2 0.25 0.4
+	double START_huf, END_huf, START_arcd, END_arcd;
+
+	char arr[] = {'a', 'b', 'c', 'd', 'e'};
+	int freq[] = {5, 10, 20, 25, 40}; // freq 0.05 0.1 0.2 0.25 0.4
 
 	int size = sizeof(arr) / sizeof(arr[0]);
 
+	START_huf = clock();
 	HuffmanCodes(arr, freq, size);
-
+	END_huf = clock();
+	cout << (END_huf - START_huf) <<"msec"<< endl;
 
 	int n = 5;
-	double freq1[] = { 0.05, 0.1, 0.2, 0.25, 0.4 };
+	double freq1[] = {0.05, 0.1, 0.2, 0.25, 0.4};
 	cout << "Enter number of characters: ";
-	//cin >> n;
+	// cin >> n;
 	unordered_map<char, node> arr1;
 	vector<char> ar;
 	double range_from = 0;
@@ -28,10 +33,10 @@ int main()
 	for (int i = 0; i < n; i++)
 	{
 		char ch;
-		//cin >> ch;
-		ch=arr[i];
+		// cin >> ch;
+		ch = arr[i];
 		ar.push_back(ch);
-		//cin >> arr[ch].prob;
+		// cin >> arr[ch].prob;
 		arr1[ch].prob = freq1[i];
 		arr1[ch].range_from = range_from;
 		arr1[ch].range_to = range_from + arr1[ch].prob;
@@ -45,10 +50,13 @@ int main()
 		cout << ch << "\t" << arr1[ch].prob << "\t\t" << arr1[ch].range_from << "\t\t" << arr1[ch].range_to << endl;
 	}
 	cout << endl;
-	string s = "aecbd";
+	string s ;
 	cout << "Enter text: ";
-	//cin >> s;
+	cin >> s;
+	START_arcd = clock();
 	double code_word = encoding(arr1, s);
+	END_arcd = clock();
+	cout << (END_arcd - START_arcd) <<"msec"<< endl;
 	cout << "Code word for " << s << " is: " << code_word << endl;
 	string text = decoding(arr1, code_word, s.size());
 	cout << "Text for " << code_word << " is: " << text << endl;
