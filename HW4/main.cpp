6d2c63187a31f663efdba7e06726b3ce8b8f70c8
timchen1216 <timchengood@gmail.com>
2022-11-26 21:31:30 +0800

    11/26

--- HW4/main.cpp
+++ HW4/main.cpp
@@ -6,30 +6,34 @@
 #include "huffman.h"
 #include "arcd.h"
 #include <time.h>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
 	double START_huf, END_huf, START_arcd, END_arcd;
 
+	cout <<"----------------------Huffman coding---------------------"<< endl;
 	char arr[] = {'a', 'b', 'c', 'd', 'e'};
 	int freq[] = {5, 10, 20, 25, 40}; // freq 0.05 0.1 0.2 0.25 0.4
-
+	cout << "Character: a b c d e\n";
+	cout << "Probability: 0.05 0.1 0.2 0.25 0.4\n";
 	int size = sizeof(arr) / sizeof(arr[0]);
 
 	START_huf = clock();
 	HuffmanCodes(arr, freq, size);
 	END_huf = clock();
-	cout << (END_huf - START_huf) <<"msec"<< endl;
+	cout <<"HuffmanCodes :"<< setprecision(5) << (END_huf - START_huf)/ CLOCKS_PER_SEC <<"sec\n\n"<< endl;
 
+	cout <<"--------------------Arithmetic coding---------------------"<< endl;
 	int n = 5;
 	double freq1[] = {0.05, 0.1, 0.2, 0.25, 0.4};
-	cout << "Enter number of characters: ";
+	//cout << "Enter number of characters: ";
 	// cin >> n;
 	unordered_map<char, node> arr1;
 	vector<char> ar;
 	double range_from = 0;
-	cout << "Enter probability of each character:\n";
+	//cout << "Enter probability of each character:\n";
 	for (int i = 0; i < n; i++)
 	{
 		char ch;
@@ -50,14 +54,14 @@ int main()
 		cout << ch << "\t" << arr1[ch].prob << "\t\t" << arr1[ch].range_from << "\t\t" << arr1[ch].range_to << endl;
 	}
 	cout << endl;
-	string s ;
-	cout << "Enter text: ";
-	cin >> s;
+	string s = "acdeb";
+	//cout << "Enter text: ";
+	//cin >> s;
 	START_arcd = clock();
 	double code_word = encoding(arr1, s);
 	END_arcd = clock();
-	cout << (END_arcd - START_arcd) <<"msec"<< endl;
 	cout << "Code word for " << s << " is: " << code_word << endl;
 	string text = decoding(arr1, code_word, s.size());
 	cout << "Text for " << code_word << " is: " << text << endl;
+	cout <<"Arithmetic coding:"<< (END_arcd - START_arcd)/ CLOCKS_PER_SEC <<"sec\n\n"<< endl;
 }
